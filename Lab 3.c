7ce02de96f86d829f5aabb4665ba80bf8f57015c
Anne Barrett <anneb1397@yahoo.com>
2018-04-25 14:32:31 -0500

    Added Lab 3 file

---
+++ Lab 3.c
@@ -0,0 +1,21 @@
+int minmax_tictactoe(int *board, int turn)
+{ 
+	//put your code here:
+	//it must return X(1), O(2) or NONE(0) for tie.
+	//use the function game_status(board), to evaluate a board
+	//it returns X(1) if X wins, O(2) if O wins or NONE(0) if tie or game is not finished
+	//the program will keep track of the number of boards evaluated
+	//int result = game_status(board);
+	return NONE;
+}
+
+int abprun_tictactoe(int *board, int turn)
+{ 
+	//put your code here:
+	//it must return X(1), O(2) or NONE(0) for tie.
+	//use the function game_status(board), to evaluate a board
+	//it returns X(1) if X wins, O(2) if O wins or NONE(0) if tie or game is not finished
+	//the program will keep track of the number of boards evaluated
+	//int result = game_status(board);
+	return NONE;
+}
